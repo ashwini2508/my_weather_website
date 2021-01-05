866034f767a92e93b1f93c868932625316f497f2
ashwini2508 <ashwinikumar25082000@gmail.com>
2021-01-06 01:48:15 +0530

    remove unnecesary console.log file

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 //const e = require("express")
 
-console.log('Client side javascript file is loaded!')
+// console.log('Client side javascript file is loaded!')
 
 // fetch('http://puzzle.mead.io/puzzle').then(response =>{
 //     response.json().then((data) =>{
